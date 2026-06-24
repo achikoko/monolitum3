5a5988f90a0557060b811ffbf996fb95ed473d68
achi <1425109@uab.cat>
2026-06-24 04:26:29 +0200

    fix: Attr_Decimal validate and stringValue

--- packages/monolitum-model/src/attr/Attr_Decimal.php
+++ packages/monolitum-model/src/attr/Attr_Decimal.php
@@ -36,10 +36,15 @@ class Attr_Decimal extends AbstractAttr
     #[\Override]
     public function validate(mixed $value): ValidatedValue
     {
-        if(is_numeric($value)){
+        if(is_float($value)){
+            // If it is a FLOAT, convert to an INT to store the value
             $withoutPoint = intval(intval($value) * pow(10, $this->decimals));
             return new ValidatedValue(true, true, $withoutPoint, null, $this->stringValue($value));
+        } if(is_int($value)){
+            // This is the CANONICAL form of a Decimal
+            return new ValidatedValue(true, true, intval($value), null, $this->stringValue($value));
         } else if(is_string($value)){
+            // If it is a string, we assume it is a FLOAT so have to multiply (forms submit the value as this)
             try{
                 $floatValue = floatval($value);
                 $intValue = intval($floatValue * pow(10, $this->decimals));
@@ -54,11 +59,15 @@ class Attr_Decimal extends AbstractAttr
     #[\Override]
     public function stringValue(mixed $value): string
     {
-        if(is_int($value)){
+        if(is_float($value)){
+            return strval($value);
+        } else if(is_int($value)){
             $zeros = pow(10, $this->decimals);
             $integerPart = intval($value / $zeros);
             $floatingPart = $value - ($integerPart * $zeros);
             return $integerPart . "." . $floatingPart;
+        } else if(is_string($value)){
+            return strval($value);
         }
         return "";
     }
