5e5707ec339bca4ba0a33f52daeae742845911e2
achi <1425109@uab.cat>
2026-07-25 14:00:19 +0200

    fix: uncommitted file

---
+++ packages/monolitum-backend/src/params/StandardProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace monolitum\backend\params;
+
+class StandardProvider
+{
+    const GET = "GET";
+    const POST = "POST";
+    const MUTABLE_GLOBALS = "DB_GLOBALS";
+
+//    case SESSION;
+//    case COOKIE;
+
+}
